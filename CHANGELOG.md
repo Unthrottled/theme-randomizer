b7a3f24043566e71f0239b38e28ec233270646a7
Alex Simons <alexsimons9999@gmail.com>
2021-02-13 07:55:47 -0600

    Better random theme distribution (#11)
    * More listening to laf changes
    
    * Updated changelog and bumped version to v0.1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 # Theme Randomizer Changelog
 
+# 0.1.3
+
+- Enhanced random theme distribution logic.
+  - Plugin will remember themes you manually set and pick accordinly so you see a theme you haven't seen in a while.
+
 # 0.1.2
 
 - Themes will _actually_ change at a more correct time when resuming code editing after your computer was suspended.
--- src/main/kotlin/io/unthrottled/theme/randomizer/PluginMaster.kt
+++ src/main/kotlin/io/unthrottled/theme/randomizer/PluginMaster.kt
@@ -6,6 +6,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.project.ProjectManagerListener
 import io.unthrottled.theme.randomizer.onboarding.UserOnBoarding
+import io.unthrottled.theme.randomizer.services.LAFProbabilityService
 import io.unthrottled.theme.randomizer.services.ThemeChangeEventEmitter
 import io.unthrottled.theme.randomizer.tools.Logging
 
@@ -14,6 +15,10 @@ class PluginMaster : ProjectManagerListener, Disposable, Logging {
   companion object {
     val instance: PluginMaster
       get() = ServiceManager.getService(PluginMaster::class.java)
+  }
+
+  init {
+    LAFProbabilityService.instance.toString() // wake up the look and feel probability service
   }
 
   private val themeChangeEventEmitter = ThemeChangeEventEmitter()
