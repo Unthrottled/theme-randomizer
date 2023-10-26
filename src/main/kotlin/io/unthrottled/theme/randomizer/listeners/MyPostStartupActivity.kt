8ffcdff344d740e89e9bd0512219616094e75b82
Elior <heliosaian@gmail.com>
2023-10-26 12:22:46 +0300

    code style

--- src/main/kotlin/io/unthrottled/theme/randomizer/listeners/MyPostStartupActivity.kt
+++ src/main/kotlin/io/unthrottled/theme/randomizer/listeners/MyPostStartupActivity.kt
@@ -1,11 +1,11 @@
 package io.unthrottled.theme.randomizer.listeners
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.startup.ProjectActivity
 import io.unthrottled.theme.randomizer.PluginMaster
 
-internal class MyPostStartupActivity : StartupActivity {
-  override fun runActivity(project: Project) {
+internal class MyPostStartupActivity : ProjectActivity {
+  override suspend fun execute(project: Project) {
     PluginMaster.instance.projectOpened()
   }
 }
