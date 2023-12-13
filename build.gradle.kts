5374175d759843515c35ac30768e3d0b4575bb2f
Christina Black <xtina@mabl.com>
2023-12-13 09:44:51 -0500

    intellij 233 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
   // Kotlin support
   id("org.jetbrains.kotlin.jvm") version "1.8.10"
   // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-  id("org.jetbrains.intellij") version "1.13.3"
+  id("org.jetbrains.intellij") version "1.16.1"
   // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
   id("org.jetbrains.changelog") version "2.0.0"
   // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
@@ -92,15 +92,15 @@ detekt {
 
 tasks {
   withType<JavaCompile> {
-    sourceCompatibility = "11"
-    targetCompatibility = "11"
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
   }
   withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "17"
   }
 
   withType<Detekt> {
-    jvmTarget = "11"
+    jvmTarget = "17"
   }
 
   runIde {
